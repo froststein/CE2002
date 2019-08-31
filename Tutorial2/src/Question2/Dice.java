37318e9b5afc580c88e24df8ed9e13168a4298ad
Froststein <froststein@DESKTOP-LOM2HU6>
2019-08-31 16:44:45 +0800

    Tut2

--- Tutorial2/src/Question2/Dice.java
+++ Tutorial2/src/Question2/Dice.java
@@ -1,5 +1,17 @@
 package Question2;
 
 public class Dice {
-
+	private int valueOfDice;
+	public Dice() {
+		
+	}
+	public void setDiceValue() {
+		
+	}
+	public int getDiceValue() {
+		return valueOfDice;
+	}
+	public void printDiceValue() {
+		
+	}
 }
--- Tutorial2/src/Question2/DiceApp.java
+++ Tutorial2/src/Question2/DiceApp.java
@@ -1,5 +1,9 @@
 package Question2;
-
+import java.util.Scanner;
 public class DiceApp {
-
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		sc.close();
+	}
 }
