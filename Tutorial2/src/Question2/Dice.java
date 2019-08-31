a38eb753b8da69e652983608faed75dd94399bf0
Froststein <Froststein@DESKTOP-LOM2HU6>
2019-08-31 16:55:37 +0800

    Tutorial 2 Done

--- Tutorial2/src/Question2/Dice.java
+++ Tutorial2/src/Question2/Dice.java
@@ -3,15 +3,15 @@ package Question2;
 public class Dice {
 	private int valueOfDice;
 	public Dice() {
-		
+		setDiceValue();
 	}
 	public void setDiceValue() {
-		
+		this.valueOfDice=(int)(Math.random()*6)+1;
 	}
 	public int getDiceValue() {
 		return valueOfDice;
 	}
 	public void printDiceValue() {
-		
+		System.out.println("Current value is : "+getDiceValue());
 	}
 }
--- Tutorial2/src/Question2/DiceApp.java
+++ Tutorial2/src/Question2/DiceApp.java
@@ -2,9 +2,21 @@ package Question2;
 import java.util.Scanner;
 public class DiceApp {
 	public static void main(String[] args) {
+		int total=0;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Please <Key> to roll the first dice");
-		
+		sc.nextLine();
+		Dice d1=new Dice();
+		d1.printDiceValue();
+		total+=d1.getDiceValue();
+
+		System.out.println("Please <Key> to roll the second dice");
+		sc.nextLine();
+		Dice d2=new Dice();
+		d2.printDiceValue();
+		total+=d2.getDiceValue();
+
+		System.out.println("Your total number is: "+total);
 		sc.close();
 	}
 }
