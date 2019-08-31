413d28770f9719f20f69d35d868397d9bb29594f
Froststein <Jia@DESKTOP-LOM2HU6>
2019-08-31 16:47:05 +0800

    Test author commit

--- Tutorial2/src/Question2/DiceApp.java
+++ Tutorial2/src/Question2/DiceApp.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class DiceApp {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Please <Key> to roll the first dice");
 		
 		sc.close();
 	}
