0c1db67f768275ffd3119d3b6ee409e88cfbbdea
Roy <froststein@users.noreply.github.com>
2019-09-07 12:21:20 +0800

    Start of lab4

--- Lab/src/Lab4/SalePerson.java
+++ Lab/src/Lab4/SalePerson.java
@@ -5,6 +5,12 @@ public class SalePerson implements Comparable {
 	private String lastName;
 	private int totalSales;
 	
+	public SalePerson(String fName, String lName, int tSales) {
+		this.firstName=fName;
+		this.lastName=lName;
+		this.totalSales=tSales;
+	}
+	
 	public String getFirstNme() {
 		return firstName;
 	}
