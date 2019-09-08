73353f17cb4c28ca26c6eaef016bc9d5f75049ca
Roy <15943697+froststein@users.noreply.github.com>
2019-09-08 09:40:32 +0800

    Created lab4 pt 2 3d app

--- Lab/src/Lab4/Shapes/Shape3DApp.java
+++ Lab/src/Lab4/Shapes/Shape3DApp.java
@@ -1,5 +1,7 @@
 package Lab4.Shapes;
 
 public class Shape3DApp {
+	public static void main(String[] args) {
 
+	}
 }
---
+++ Lab/src/Lab4/Shapes/Sphere.java
@@ -0,0 +1,17 @@
+package Lab4.Shapes;
+
+public class Sphere extends Shape {
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void getDimensions() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
